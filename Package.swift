9068940177689c0c3dcf3452ab3db200cb7e4db2
Mobile-仲立 <bit_lai@mail.chungyo.net>
2025-04-01 10:43:30 +0800

    update checksum

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .binaryTarget(
         name: "HevSocks5Tunnel",
         url: "https://github.com/ghostwolf90/Tun2SocksKit/releases/download/2.2.2/HevSocks5Tunnel.xcframework.zip",
-        checksum: "f5a0edb493fd689c871e109f47eb8959bbe02b4a7d94a827df97dd47e8a1bba6"
+        checksum: "d18929d8fe0d3df478bb4af2a172376ab16a1d706c12de593073cca66b091a04"
     )
   ]
 )
